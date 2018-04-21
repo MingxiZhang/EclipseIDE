58ec41b6b003d166edbc9a08d9a84d7ab7dd2270
mingxi zhang <bill.zhang@zafin.com>
2018-04-21 17:04:17 -0400

    First commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/64/30cc6bac0c45001812dd857f36dffdcc
@@ -0,0 +1,6 @@
+
+public class ImmutableSetTest {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
---
+++ ImmutableCollection/src/main/java/com/zmx/guavaTest/ImmutableCollection/App.java
@@ -0,0 +1,14 @@
+package com.zmx.guavaTest.ImmutableCollection;
+
+import com.google.common.collect.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
